02cffe161288e41a57923bd466d32b871846f6cb
permissionX <buaaxuke@gmail.com>
2019-12-01 23:28:01 +0800

    switch boundary condition

--- source/BoundaryCondition.jl
+++ source/BoundaryCondition.jl
@@ -1,13 +1,12 @@
 module OpenBoundaryCondition
     export DVector, Distance, Shift!
-    using Main
 
-    function DVector(obj1::Main.DefectObj, obj2::Main.DefectObj, universe::Main.Universe)   
+    function DVector(obj1::Base.Main.DefectObj, obj2::Base.Main.DefectObj, universe::Base.Main.Universe)   
         dr = obj2.position - obj1.position
         dr
     end
 
-    function Distance(obj1::Main.DefectObj, obj2::Main.DefectObj, universe::Main.Universe)
+    function Distance(obj1::Base.Main.DefectObj, obj2::Base.Main.DefectObj, universe::Base.Main.Universe)
         dVector = DVector(obj1, obj2, universe)
         distance = 0.0
         for d in 1:universe.box.nDimension
@@ -16,15 +15,15 @@ module OpenBoundaryCondition
         return distance = distance^0.5
     end
 
-    function Shift!(obj::Main.DefectObj, dr::Main.Vector{Int64}, universe::Main.Universe)
+    function Shift!(obj::Base.Main.DefectObj, dr::Base.Main.Vector{Int64}, universe::Base.Main.Universe)
         obj.position += dr
         for d in 1:universe.box.nDimension
             if obj.position[d] < universe.box.boundary[d,1]
-                Main.Introduce!(universe, [Main.Disappear()], obj)
+                Base.Main.Introduce!(universe, [Base.Main.Disappear()], obj)
                 #obj.position[d] += universe.box.length[d]
             elseif obj.position[d] >= universe.box.boundary[d,2]
                 #obj.position[d] -= universe.box.length[d]
-                Main.Introduce!(universe, [Main.Disappear()], obj)
+                Base.Main.Introduce!(universe, [Base.Main.Disappear()], obj)
             end
         end
     end
@@ -32,9 +31,8 @@ end
 
 module PPPBoundaryCondition
     export DVector, Distance, Shift!
-    using Main
 
-    function DVector(obj1::Main.DefectObj, obj2::Main.DefectObj, universe::Main.Universe)   
+    function DVector(obj1::Base.Main.DefectObj, obj2::Base.Main.DefectObj, universe::Base.Main.Universe)   
         dr = obj2.position - obj1.position
         for d in 1:universe.box.nDimension
             if dr[d] >= universe.box.halfLength[d]
@@ -46,7 +44,7 @@ module PPPBoundaryCondition
         dr
     end
 
-    function Distance(obj1::Main.DefectObj, obj2::Main.DefectObj, universe::Main.Universe)
+    function Distance(obj1::Base.Main.DefectObj, obj2::Base.Main.DefectObj, universe::Base.Main.Universe)
         dVector = DVector(obj1, obj2, universe)
         distance = 0.0
         for d in 1:universe.box.nDimension
@@ -55,7 +53,7 @@ module PPPBoundaryCondition
         return distance = distance^0.5
     end
 
-    function Shift!(obj::Main.DefectObj, dr::Main.Vector{Int64}, universe::Main.Universe)
+    function Shift!(obj::Base.Main.DefectObj, dr::Base.Main.Vector{Int64}, universe::Base.Main.Universe)
         obj.position += dr
         for d in 1:universe.box.nDimension
             if obj.position[d] < universe.box.boundary[d,1]
